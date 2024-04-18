5cea8585f6773fdf93a3632ed58d384261188cc4
wangyue <wangy@bokecc.com>
2024-04-18 16:06:15 +0800

    升级1.1.5

---
+++ app/src/main/java/com/bokecc/ccsskt/example/CCApplication.java
@@ -0,0 +1,28 @@
+package com.bokecc.ccsskt.example;
+
+import android.app.Application;
+
+import com.bokecc.common.application.ApplicationData;
+
+/**
+ * Application
+ */
+public class CCApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //公共库初始化
+        ApplicationData.getInstance().init(this,true);
+    }
+
+    /**
+     * 程序终止的时候执行
+     */
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        ApplicationData.getInstance().onTerminate();
+    }
+
+}
